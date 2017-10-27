002fcb846a9a77760471c628f2e79a31e2c43d0e
Hannah Danziger <hdanziger8@gmail.com>
2017-10-27 16:04:21 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,7 +35,7 @@ class Student
       VALUES (?, ?)
       SQL
       DB[:conn].execute(sql, self.name, self.grade)
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[index.first]
+      @id = DB[:conn].execute("INSERT last_insert_rowid() FROM students")[0][0]
     end
   end
 
