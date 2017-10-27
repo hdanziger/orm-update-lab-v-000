e512461f653fc67c1ee5feccb00d5eb9879a8739
Hannah Danziger <hdanziger8@gmail.com>
2017-10-27 16:10:26 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,8 +58,10 @@ class Student
     FROM students
     WHERE name = ?
     SQL
-    result = DB[:conn].execute(sql, name)[0]
-    Student.new(result[0], result[1], result[2])
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
   def update
