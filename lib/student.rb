a5806c0ee1f386f6864aa91a12350f5d859ad1ac
Hannah Danziger <hdanziger8@gmail.com>
2017-10-27 16:08:23 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,7 +57,6 @@ class Student
     SELECT *
     FROM students
     WHERE name = ?
-    LIMIT 1
     SQL
     result = DB[:conn].execute(sql, name)[0]
     Student.new(result[0], result[1], result[2])
