e2f05034b22aeabcf463f1c3fcc6205e6cb8a5d3
Hannah Danziger <hdanziger8@gmail.com>
2017-10-27 15:38:12 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,6 +2,11 @@ require_relative "../config/environment.rb"
 
 class Student
 
+  def initialize (name, grade, id=nil)
+    @name = name 
+    @grade = grade 
+  end
+
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
